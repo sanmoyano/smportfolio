4e81024cf687769ab63f091a83f458a4649d398a
sanmoyano <smoyano.di@gmail.com>
2022-04-19 00:17:03 -0300

    project update

--- src/ui/layout/3.Projects/Project.js
+++ src/ui/layout/3.Projects/Project.js
@@ -1,21 +1,38 @@
-import { Box, Image, HStack, Stack } from "@chakra-ui/react";
+import { Box, Image, HStack, Stack, Text, Button } from "@chakra-ui/react";
 
 const Project = ({ project }) => {
     const skillsArray = project.skills;
 
     return (
-        <Stack className="card__project" spacing={4}>
+        <Stack className="project__card" spacing={4}>
             <h3>{project.title}</h3>
-            <Box className="project__content">
+            <Box spacing={4}>
                 <Image alt={project.alt} borderRadius="md" src={project.img} />
-                <p>{project.description}</p>
+                <Text marginTop={4}>{project.description}</Text>
             </Box>
-            <HStack className="project__skills">
-                {skillsArray.map((skill) => (
-                    <Box key={skill} spacing={4}>
-                        {skill}
-                    </Box>
-                ))}
+            <HStack className="project__skills" display="flex" justifyContent={"space-between"}>
+                <HStack>
+                    {skillsArray.map((skill) => (
+                        <Box key={skill} color="whitesmoke" fontSize="1.6rem" spacing={4}>
+                            [{skill}]
+                        </Box>
+                    ))}
+                </HStack>
+                <Button
+                    _hover={{ bg: "whitesmoke", color: "#0d0d0d" }}
+                    bg="#adff2f"
+                    fontFamily={`"JetBrains Mono", monospace`}
+                    fontSize="1.6rem"
+                    paddingInline={12}
+                    size="lg"
+                    textAlign="center"
+                    textColor="#0d0d0d"
+                    variant="solid"
+                >
+                    <a href={project.link} rel="noreferrer" target="_blank">
+                        visit
+                    </a>
+                </Button>
             </HStack>
         </Stack>
     );
